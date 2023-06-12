28dd516f4b540576395977509be5f7c4f331c53c
ashlesss <hidahza@gmail.com>
2023-06-12 00:08:01 -0400

    add copyright info

--- src/pages/IntroPage.vue
+++ src/pages/IntroPage.vue
@@ -1,6 +1,11 @@
 <template>
   <q-page class="flex flex-center">
-    <UsageIntro />
+    <div class="column">
+      <UsageIntro />
+      <div class="flex flex-center absolute-bottom" style="font-size: 1rem;">
+        This project is built by&nbsp;<a href="https://ashless.io" target="_blank">Ashless</a>&nbsp;and using&nbsp;<a href="https://quasar.dev/" target="_blank">Quasar Framework</a>
+      </div>
+    </div>
   </q-page>
 </template>
 
